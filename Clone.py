c93982ddacdacd2043c036b815456ceeeaa806e9
your1name <nguyenthanhxxxx0912@gmail.com>
2022-12-02 21:44:39 +0700

    draw ghost class

--- Clone.py
+++ Clone.py
@@ -1,6 +1,7 @@
 import pygame, sys, math
 import math
 from pygame.locals import *
+from random import randrange
 
 
 # 0 = empty black rectangle, 1 = dot, 2 = big dot, 3 = vertical line,
@@ -65,66 +66,33 @@ player_x =  345
 player_y = 621
 flicker = False
 
-
+level = board
+PINK = (230, 200, 158)
+score = 0
+lives = 3 
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
 
-level = board
-PINK = (230, 200, 158)
-
-def draw_board():
-
-    for i in range(len(level)):
-        for j in range(len(level[i])):
-            pygame.draw.rect(screen,(255,255,255),(j*PIXEL, i * PIXEL, 23, 23),1)
-
-            if level[i][j] == 1: # tik-tak
-                pygame.draw.circle(screen, PINK, (j * PIXEL + (0.5 * PIXEL), i * PIXEL + (0.5 * PIXEL)), 4)
-
-            if level[i][j] == 2 and not flicker : # special tik-tak
-                pygame.draw.circle(screen, PINK, (j * PIXEL + (0.5 * PIXEL), i * PIXEL + (0.5 * PIXEL)), 10)
-
-            if level[i][j] == 3:  # line Y
-                pygame.draw.line(screen, 'blue', (j * PIXEL + (0.5 * PIXEL), i * PIXEL),(j * PIXEL + (0.5 * PIXEL), i * PIXEL + PIXEL), 3) # line từ A (x,y) tới B (x,y)
-                
-            if level[i][j] == 4:  # line X
-                pygame.draw.line(screen, 'blue', (j * PIXEL, i * PIXEL + (0.5 * PIXEL)),(j * PIXEL + PIXEL, i * PIXEL + (0.5 * PIXEL)), 3)  # line từ A (x,y) tới B (x,y)
-
-            if level[i][j] == 5: # góc phần tư thứ 1 của Unit circle
-                pygame.draw.arc(screen, 'blue', [(j * PIXEL - (PIXEL * 0.4)) - 2, (i * PIXEL + (0.5 * PIXEL)), PIXEL, PIXEL], 0, PI / 2, 3)
-
-            if level[i][j] == 6: # góc phần tư thứ 2 của Unit circle
-                pygame.draw.arc(screen, 'blue',[(j * PIXEL + (PIXEL * 0.5)), (i * PIXEL + (0.5 * PIXEL)), PIXEL, PIXEL], PI / 2, PI, 3)
-
-            if level[i][j] == 7: # góc phần tư thứ 3 của Unit circle
-                pygame.draw.arc(screen, 'blue', [(j * PIXEL + (PIXEL * 0.5)), (i * PIXEL - (0.4 * PIXEL)), PIXEL, PIXEL], PI,3 * PI / 2, 3,)
-
-            if level[i][j] == 8: # góc phần tư thứ 4 của Unit circle
-                pygame.draw.arc(screen, 'blue',[(j * PIXEL - (PIXEL * 0.4)) - 2, (i * PIXEL - (0.4 * PIXEL)), PIXEL, PIXEL], 3 * PI / 2,2 * PI, 3)
-
-            if level[i][j] == 9: # cổng ma 
-                pygame.draw.line(screen, 'white', (j * PIXEL, i * PIXEL + (0.5 * PIXEL)),(j * PIXEL + PIXEL, i * PIXEL + (0.5 * PIXEL)), 3)
-
 class Pacman():
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.pacSpeed = 1/8
+        self.pacSpeed = 1/16
         self.dir = 0 # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
         self.newDir = 0        
 
-    def draw(self):
+    def draw(self): 
     # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
         if self.dir == 0:
-            screen.blit(player_images[counter // 5], (self.col*23, self.row*23))
+            screen.blit(player_images[counter // 8], (self.col*23, self.row*23))
         elif self.dir == 1:
-            screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (self.col*23, self.row*23))
+            screen.blit(pygame.transform.flip(player_images[counter // 8], True, False), (self.col*23, self.row*23))
         elif self.dir == 2:
-            screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (self.col*23, self.row*23))
+            screen.blit(pygame.transform.rotate(player_images[counter // 8], 90), (self.col*23, self.row*23))
         elif self.dir == 3:
-            screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (self.col*23, self.row*23))     
+            screen.blit(pygame.transform.rotate(player_images[counter // 8], 270), (self.col*23, self.row*23))     
    
     def update(self):
         if self.newDir == 2:
@@ -160,22 +128,117 @@ class Pacman():
         elif self.dir == 1:
             if canMove(self.row, math.floor(self.col - self.pacSpeed)) and self.row % 1.0 == 0:
                 self.col -= self.pacSpeed
+
+class Ghost():
+    def __init__(self, row, col, color) :
+        self.row = row
+        self.col = col
+        self.color = color
+
+        self.attacked = False
+        # thời gian power
+        self.dir = randrange(4)
+        self.dead = False
+        self.deathTimer = 120
+        self.ghostSpeed = 1/4
+
+    def draw(self):
+        ghostImage = pygame.image.load(f'assets/ghost_images/dead.png')
         
+        if self.dead:
+            ghostImage = pygame.image.load(f'assets/ghost_images/dead.png')
+        elif self.attacked:
+            pygame.image.load(f'assets/ghost_images/powerup.png') # spooked
+        else:
+            if self.color == "blue":
+                ghostImage = pygame.image.load(f'assets/ghost_images/blue.png')
+            elif self.color == "pink":
+                ghostImage = pygame.image.load(f'assets/ghost_images/pink.png')
+            elif self.color == "orange":
+                ghostImage = pygame.image.load(f'assets/ghost_images/orange.png')
+            elif self.color == "red":
+                ghostImage = pygame.image.load(f'assets/ghost_images/red.png')
+        
+        ghostImage = pygame.transform.scale(ghostImage, (30,30))
+        screen.blit(ghostImage, (self.col *23, self.row * 23))
+    
+    def setAttacked(self, isAttacked):
+        self.attacked = isAttacked
+
+    def isAttacked(self):
+        return self.attacked
+
+    def setDead(self, isDead):
+        self.dead = isDead
+
+    def isDead(self):
+        return self.dead
+
+
+
+def draw_board():
+
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            #pygame.draw.rect(screen,(255,255,255),(j*PIXEL, i * PIXEL, 23, 23),1)
+
+            if level[i][j] == 1: # tik-tak
+                pygame.draw.circle(screen, PINK, (j * PIXEL + (0.5 * PIXEL), i * PIXEL + (0.5 * PIXEL)), 4)
+
+            if level[i][j] == 2 and not flicker : # special tik-tak
+                pygame.draw.circle(screen, PINK, (j * PIXEL + (0.5 * PIXEL), i * PIXEL + (0.5 * PIXEL)), 10)
+
+            if level[i][j] == 3:  # line Y
+                pygame.draw.line(screen, 'blue', (j * PIXEL + (0.5 * PIXEL), i * PIXEL),(j * PIXEL + (0.5 * PIXEL), i * PIXEL + PIXEL), 3) # line từ A (x,y) tới B (x,y)
+                
+            if level[i][j] == 4:  # line X
+                pygame.draw.line(screen, 'blue', (j * PIXEL, i * PIXEL + (0.5 * PIXEL)),(j * PIXEL + PIXEL, i * PIXEL + (0.5 * PIXEL)), 3)  # line từ A (x,y) tới B (x,y)
+
+            if level[i][j] == 5: # góc phần tư thứ 1 của Unit circle
+                pygame.draw.arc(screen, 'blue', [(j * PIXEL - (PIXEL * 0.4)) - 2, (i * PIXEL + (0.5 * PIXEL)), PIXEL, PIXEL], 0, PI / 2, 3)
+
+            if level[i][j] == 6: # góc phần tư thứ 2 của Unit circle
+                pygame.draw.arc(screen, 'blue',[(j * PIXEL + (PIXEL * 0.5)), (i * PIXEL + (0.5 * PIXEL)), PIXEL, PIXEL], PI / 2, PI, 3)
+
+            if level[i][j] == 7: # góc phần tư thứ 3 của Unit circle
+                pygame.draw.arc(screen, 'blue', [(j * PIXEL + (PIXEL * 0.5)), (i * PIXEL - (0.4 * PIXEL)), PIXEL, PIXEL], PI,3 * PI / 2, 3,)
+
+            if level[i][j] == 8: # góc phần tư thứ 4 của Unit circle
+                pygame.draw.arc(screen, 'blue',[(j * PIXEL - (PIXEL * 0.4)) - 2, (i * PIXEL - (0.4 * PIXEL)), PIXEL, PIXEL], 3 * PI / 2,2 * PI, 3)
+
+            if level[i][j] == 9: # cổng ma 
+                pygame.draw.line(screen, 'white', (j * PIXEL, i * PIXEL + (0.5 * PIXEL)),(j * PIXEL + PIXEL, i * PIXEL + (0.5 * PIXEL)), 3)
+     
 def canMove(row, col):
-    if board[int(row)][int(col)] < 3:
+    if col <= -1 or col == len(level[0]):
+        return True
+    if level[int(row)][int(col)] < 3:
         return True
     return False
 
+def check_collisions(score):
+    if pac.row % 1.0 == 0 and pac.col % 1.0 == 0:
+        if level[int(pac.row)][int(pac.col)] == 1:
+            level[int(pac.row)][int(pac.col)] = 0
+            score += 10
+        if level[int(pac.row)][int(pac.col)] == 2:
+            level[int(pac.row)][int(pac.col)] = 0
+            score += 50
+           
+    return score
 
+def draw_live():
+    #pygame.draw.rect(screen,(255,255,255),(30 , 835, 23, 23),1)
+    for i in range(lives):
+        screen.blit(pygame.transform.scale(player_images[0], (25, 25)), (30 + i * 40, 830))
 
 pac = Pacman(27,15) 
-
-
+ghosts = [Ghost(18,12,'blue'), Ghost(18,14,'red'), Ghost(18,15,'pink'), Ghost(18,17,"orange") ]
 while True:
     timer.tick(60) # fps cua game
-    if counter < 19:
+    if counter < 31:
         counter += 1
-        if counter > 5:
+        if counter > 8:
             flicker = False
     else:
         counter = 0
@@ -186,14 +249,19 @@ while True:
 
     pac.update()
     pac.draw()
-    #print(pac.row, pac.col)
-    #pac.col %= len(board[0]) 
+    #print(pac.row, pac.col, f'score: {score}')
+    for ghost in ghosts:
+            ghost.draw()
+
     
     if pac.col >= 29:
         pac.col = 0
     
     if pac.col < 0:
         pac.col = 29
+
+    score = check_collisions(score)
+    draw_live()
     
     for event in pygame.event.get():
         if event.type == QUIT:
